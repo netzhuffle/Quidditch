03e65ab47adee052bc1cb3d9c95aec65ee320be4
Jannis Grimm <jannis@gje.ch>
2013-02-25 11:37:50 +0100

    Use composers autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,3 @@
 <?php
 
-require_once 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace('Netzhuffle', __DIR__);
-$loader->register();
+require_once 'vendor/autoload.php';
